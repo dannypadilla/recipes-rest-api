19d752d48750aff4d21e908df56697bbfbfd6650
Danny Padilla <dpadilla010@gmail.com>
2021-11-21 19:54:39 -0800

    MOD - format json output

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET recipes listing. */
 router.get('/', async function(req, res, next) {
     let db_collection_name = req.app.locals.db_collection;
-    let users = await req.app.locals.db.collection(db_collection_name);
-    res.json( await users.find().toArray() );
+    let db_recipes = await req.app.locals.db.collection(db_collection_name);
+
+    let projection = {_id: false, name: true};
+
+    res.json( await db_recipes.find().project(projection).toArray() );
     //res.send('respond with a resource');
 });
 
