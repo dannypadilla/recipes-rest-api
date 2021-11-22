ccdccb58737d90d8fe6af4befed4062154494a55
Danny Padilla <dpadilla010@gmail.com>
2021-11-21 22:32:03 -0800

    ADD - add a recipe route

--- routes/recipes.js
+++ routes/recipes.js
@@ -65,4 +65,46 @@ router.post("/ingredients", async function(req, res, next) {
 
 });
 
+
+/**
+ * 4. Add a recipe.
+ */
+ router.post("/add", async function(req, res, next) {
+
+    // inputs
+    let recipe = req.body.recipe;
+    let name = req.body.name;
+    let yield = req.body.yield;
+    let ingredients_names = req.body.ingredients_name;
+    let ingredients_qty = req.body.ingredients_qty;
+    let directions = req.body.directions;
+
+    let num_of_ingredients = ingredients_names.length;
+    let ingredients_formatted = [];
+
+    // prepare ingredients list from POST
+    for (let i = 0; i < num_of_ingredients; i++) {
+        let name = ingredients_names[i];
+        let qty = ingredients_qty[i];
+        ingredients_formatted.push( {"name": name, "qty": qty} );
+    }
+
+    let recipe_formatted = {
+        "name": name,
+        "yield": yield,
+        "ingredients": ingredients_formatted,
+        "directions": directions
+    };
+
+    console.log(recipe_formatted);
+
+    let db_collection_name = req.app.locals.db_collection;
+    let db_recipes = await req.app.locals.db.collection(db_collection_name);
+    //let projection = {_id: false, name: true, ingredients: true};
+    await db_recipes.insertOne(recipe_formatted);
+
+    res.json(recipe_formatted);
+
+});
+
 module.exports = router;
